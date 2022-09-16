e595b87f4e95701e68b34e0a7af11e519d3d2c4e
mskoldSU <skoldmobile@gmail.com>
2022-09-16 08:38:11 +0200

    quiet as default

--- R/pd_SWE_counties.R
+++ R/pd_SWE_counties.R
@@ -6,6 +6,6 @@
 #' @return An sf polygon
 #' @export
 pd_SWE_counties <- function(county_name = NA, county_nr = NA, ...){
-  SWE_counties <- sf::st_read(system.file("LanSweref99TM", "Lan_Sweref99TM_region.shp", package = "popdensity"), ...)
+  SWE_counties <- sf::st_read(system.file("LanSweref99TM", "Lan_Sweref99TM_region.shp", package = "popdensity"), quiet = TRUE, ...)
   dplyr::filter(SWE_counties, stringr::str_detect(LnNamn, paste0(county_name, collapse = "|")) | (LnKod %in% county_nr))
 }
